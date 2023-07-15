ff67e008e876bb4d407a27dc8e7eb5e322f6e1a1
Romakumari17 <138012027+Romakumari17@users.noreply.github.com>
2023-07-15 09:46:32 +0530

    random ()

--- app/src/main/java/com/romakumari/alertdialogtext/MainActivity.kt
+++ app/src/main/java/com/romakumari/alertdialogtext/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.romakumari.alertdialogtext
 
+import android.annotation.SuppressLint
+import android.icu.text.Edits
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
-    var tvRandom:TextView?=null
+    private var etRandom:EditText?=null
     var btnRandomAlert: Button?=null
+    var tvRandom:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etRandom=findViewById(R.id.etRandom)
         tvRandom=findViewById(R.id.tvRandom)
         btnRandomAlert=findViewById(R.id.btnRandomAlert)
         btnRandomAlert?.setOnClickListener {
@@ -25,14 +30,14 @@ class MainActivity : AppCompatActivity() {
             for (i in 0..2)
             {
                 val rand=('a'..'z').random()
-                tvRandom?.setText(tvRandom?.text.toString()+rand.toString())
+                etRandom?.setText(etRandom?.text.toString()+rand.toString())
             }
         }
                     .setPositiveButton("4"){_,_,->
                         Toast.makeText(this,"clicked 4",Toast.LENGTH_SHORT).show()
                         for (i in 0..3){
                             val rand2=('a'..'z').random()
-                            tvRandom?.setText(tvRandom?.text.toString()+rand2.toString())
+                            etRandom?.setText(etRandom?.text.toString()+rand2.toString())
 
                         }
 
